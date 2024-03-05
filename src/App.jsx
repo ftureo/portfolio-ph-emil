45caf92e9b4de4a752434ffb0a70b34e960df86e
Fabián Tureo <ftureo@lanacion.com.ar>
2024-03-05 01:29:24 -0300

    feat: ✨ add base components and pages to develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+// Import to use React Router DOM and routing completely
+import { Routes, Route } from "react-router-dom";
+
 function App() {
     return (
-        <>
-            {/* Implement stash develop */}
-        </>
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <h1 className="text-3xl font-bold underline">
+                            React App
+                        </h1>
+                    </>
+                }
+            />
+            {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
     );
 }
 
