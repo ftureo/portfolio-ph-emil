490e9a9c35117505793347fdd6dc3a1a851248bc
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-09 16:48:14 -0300

    add: ✨ Apply render album by data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 import { Layout } from "../components/Layout";
+import { Album } from "../components/Album";
+import { albumData } from "../utils/album-data";
 
 export const Home = () => {
-    return <Layout/>;
+    console.log(albumData);
+    return (
+        <Layout>
+            <main className="container mx-auto">
+                {albumData.map((album, index) => {
+                    return <Album album={album} index={index} key={index} />;
+                })}
+            </main>
+        </Layout>
+    );
 };
