66e619bf79e74c2ff29a77a62bce63bcfd46954e
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-09 03:45:25 -0300

    add: ✨ Add links in navbar includes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
+import { Link } from "react-router-dom";
+import { paths } from "../routes/paths";
+
 export const Header = () => {
     return (
-        <>
+        <header>
+            <div>
+                <Link to={paths.HOME}>
+                    <img src="https://cdn-icons-png.flaticon.com/256/5904/5904483.png" />
+                </Link>
+            </div>
             <nav>
                 <ul>
                     <li>
-                        <a href="/">Home</a>
+                        <Link to={paths.MAIN_ALBUM}>Principal</Link>
                     </li>
                     <li>
-                        <a href="/about-me">About Me</a>
+                        <Link to={paths.SPECIAL_ALBUM}>Especial</Link>
+                    </li>
+                    <li>
+                        <Link to={paths.ABOUT_ME}>Sobre mí</Link>
+                    </li>
+                    <li>
+                        <Link to={paths.SHOP}>Shop</Link>
                     </li>
                 </ul>
             </nav>
-        </>
+        </header>
     );
 };
