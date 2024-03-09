f7e571a533a43c794f5402fe1c72c6f79aee6624
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-09 03:14:26 -0300

    add: ✨ implement index to export pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import { Home } from "./Home";
+import { AboutMe } from "./About-me";
+import { NotFound } from "./NotFound";
+import { MainAlbum } from "./MainAlbum";
+import { SpecialAlbum } from "./SpecialAlbum";
+import { Shop } from "./Shop";
+
+export { Home, AboutMe, NotFound, MainAlbum, SpecialAlbum, Shop };
