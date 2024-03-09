584945b326981034658ae09586ae984282c956f7
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-09 03:13:24 -0300

    fix: 🐛 replace approach using createRouteProvider to use Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { Layout } from "./components/Layout";
-import { NotFound } from "./pages/NotFound";
-import routes from "./routes";
+import { paths } from "./routes/paths";
+import {
+    Home,
+    AboutMe,
+    MainAlbum,
+    NotFound,
+    Shop,
+    SpecialAlbum,
+} from "./pages";
 
 function App() {
-    console.log({routes});
-    const router = createBrowserRouter([
-        {
-            element: <Layout />,
-            errorElement: <NotFound />,
-            children: routes,
-        },
-    ]);
-    return <RouterProvider router={router} />;
+    return (
+        <Routes>
+            <Route path={paths.HOME} element={<Home />} />
+            <Route path={paths.ABOUT_ME} element={<AboutMe />} />
+            <Route
+                path={paths.MAIN_ALBUM}
+                element={<MainAlbum title="Main Title" />}
+            />
+            <Route path={paths.SPECIAL_ALBUM} element={<SpecialAlbum />} />
+            <Route path={paths.SHOP} element={<Shop />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default App;
