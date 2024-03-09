3500a80920e3165e705daf863397a068742fcfb4
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-09 16:47:23 -0300

    fix: 🐛 Replace with Albumes component instead of Main and Special album

--- src/pages/Albumes.jsx
+++ src/pages/Albumes.jsx
@@ -1,5 +1,5 @@
 import { Layout } from "../components/Layout";
-export const SpecialAlbum = () => {
+export const Albumes = () => {
     return <Layout>
         <h1>Special Album</h1>
         <p>Special Album is a collection of photos from the most special moments of my life.</p>
--- src/pages/MainAlbum.jsx
+++
@@ -1,5 +0,0 @@
-import { Layout } from "../components/Layout";
-
-export const MainAlbum = () => {
-    return <Layout />;
-};
